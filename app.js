3019de5b01e44954b6c8e19fbbe2d6953903709f
Amber <Amberrmcclendon@gmail.com>
2021-07-18 20:41:47 -0400

    hwes6

---
+++ app.js
@@ -0,0 +1,33 @@
+var a= 1;
+var b =2;
+function add(a,b){
+return a + b;
+}
+console.log (add(a,b));
+addAnother = (a,b) => {
+    return a + b;
+}
+console.log(addAnother(a,b));
+
+function subtract(a,b){
+    return a - b;
+}
+console.log(subtract(a,b));
+subtractHere = (a,b) => {
+    return a - b;
+}
+
+function divide(a,b){
+    return a / b;
+}
+console.log(divide(a,b));
+divideThis = (a,b) => {
+    return a / b;
+}
+function multiply (a,b){
+    return a * b;
+} 
+console.log (multiply(a,b));
+multiplyHere =(a,b) => {
+    return a * b;
+}
